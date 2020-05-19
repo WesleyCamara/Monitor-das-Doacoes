f2e3e6fc6e6aecafbbb4e368faf3bcb400348135
WesleyCamara <wesleycamara@hotmail.com.br>
2020-05-19 20:02:20 -0300

    Criada a estrutura HTML inicial

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import GraficoIndicadores from './components/Graficos-indicadores'
 
 ReactDOM.render(
+  <>
     <App />
-,
+    <GraficoIndicadores />
+  </>
+  ,
   document.getElementById('root')
 );
 
