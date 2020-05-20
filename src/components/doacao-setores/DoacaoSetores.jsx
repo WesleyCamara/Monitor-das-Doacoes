8c044d4cce1374cd15ba68a0f0ca1d8cd4a82938
Harumi <haruumi.nishimura@gmail.com>
2020-05-19 21:17:56 -0300

    estrtura basica doacao setores

---
+++ src/components/doacao-setores/DoacaoSetores.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class DoacaoSetores extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>Doação por Setores</h1>
+
+                <div id="grafico">
+                    <h2>Gráfico</h2>
+                </div>
+
+                <button>VER DETALHES</button>
+            </div>
+        )
+    }
+}
+
+export default DoacaoSetores
