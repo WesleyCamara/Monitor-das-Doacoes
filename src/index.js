2f55076ba776d485ef17a212125eeed64686cd9a
Rafael <rafaelrodrigues_@outlook.com>
2020-05-21 12:32:28 -0300

    Adcionado componentes Menu e Footer no render

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Footer from './components/footer/Footer'
+import Menu from './components/menu/Menu'
+
+const elemento = document.getElementById('root');
 
 ReactDOM.render(
+  <div>
+    <Menu />
     <App />
-,
-  document.getElementById('root')
-);
+    <Footer />
+  </div>
+    
+, elemento);
 
 
