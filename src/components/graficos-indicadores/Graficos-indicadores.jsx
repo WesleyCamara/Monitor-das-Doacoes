779883d43df4fecfd8748cb4f00d6cf9da1107f6
Wesley Camara <wesleycamara@hotmail.com.br>
2020-05-22 15:57:08 -0300

    Estilização desktop ok

--- src/components/graficos-indicadores/Graficos-indicadores.jsx
+++ src/components/graficos-indicadores/Graficos-indicadores.jsx
@@ -1,30 +1,42 @@
 import React from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
-import './Graficos-indicadores.css'
+import "./Graficos-indicadores.css";
+import handMoney from '../../assets/img/graficos-indicadores/hand-money.png';
 
-
+Highcharts.setOptions({
+  lang: {
+    thousandsSep: ".",
+  },
+});
 
 const options = {
   chart: {
     type: "column",
     backgroundColor: "#F3F3F3",
     
-
   },
   colors: ["#4DB6AC"],
   title: {
     text: "",
   },
+  plotOptions: {
+    series: {
+      groupPadding: 0,
+      borderColor: 'none'
+    },
+  },
   xAxis: {
     type: "category",
+    lineWidth: 1,
+    lineColor: "#707070",
     labels: {
       rotation: 0,
-
       style: {
-        fontSize: "13px",
+        fontSize: "12px",
         fontFamily: "rubik, sans-serif",
-        
+        width: 9,
+        textOverflow: "auto",
       },
     },
   },
@@ -33,9 +45,10 @@ const options = {
     title: {
       text: "",
     },
-    format: "{point.y:,.1f}",
-    gridLineDashStyle: 'Dash',
-    
+
+    gridLineDashStyle: "Dash",
+    lineWidth: 2,
+    lineColor: "#222222",
   },
   legend: {
     enabled: false,
@@ -43,6 +56,7 @@ const options = {
   tooltip: {
     enable: false,
   },
+
   series: [
     {
       name: "Valor doado",
@@ -64,12 +78,9 @@ const options = {
         enabled: true,
         rotation: -90,
         color: "#222222",
-
-        // align: 'right',
-        format: "{point.y:,.0f}", // one decimal
         y: 65, // 10 pixels down from the top
         style: {
-          fontSize: "1em",
+          fontSize: "1.25em",
           fontFamily: "rubik, sans-serif",
           fontWeight: "400",
           textOutline: "none",
@@ -83,78 +94,100 @@ function GraficoIndicadores() {
   return (
     <section className="section-grafico-container">
       <div className="grafico-indicadores">
-      <div className="div-grafico">
-        <h2 className="titulo">Empresas com mais doações</h2>
-        <div className="grafico">
-        <HighchartsReact highcharts={Highcharts} options={options} />
-        </div>
-        <div className="grafico-indicadores-botao">ver detalhes</div>
-      </div>
-
-      <div className="div-indicadores">
-        <h2 className="titulo">Resumo</h2>
-
-        <div className="indicadores-subitem">
-          <div >
-            <h3> Doações (mínimo R$3.000)</h3>
-            <span className="span-h3">R$ 3.853.726.767</span>
+        <div className="div-grafico">
+          <h2 className="titulo">Empresas com mais doações</h2>
+          <div className="grafico">
+            <HighchartsReact highcharts={Highcharts} options={options} />
           </div>
-
-          <div className="indicadores-subitem-doadores">
-            <p>
-              Total de doadores <br />
-              <span>210</span>
-            </p>
-
-            <p>
-              Maior doador: Itaú <br />
-              <span>R$ 1.127.100.000</span>
-              <span>(32%)</span>
-            </p>
-          </div>
+          <div className="grafico-indicadores-botao">ver detalhes</div>
         </div>
 
-        <div className="indicadores-subitem">
-          <div >
-            <h3>Capanhas Doações </h3>
-            <span className="span-h3">R$ 278.275.247</span>
+        <div className="div-indicadores">
+          <h2 className="titulo">Resumo</h2>
+
+          <div className="indicadores-subitem">
+          <div><img className="imagem-mao-dinheiro" src={handMoney} /></div>
+            <div>
+           <div>
+              <h3>Doações (mínimo R$3.000) </h3>
+              <h3>
+                <span className="span-h3">R$ 3.853.726.767</span>
+              </h3>
+            </div>
+
+            <div className="indicadores-subitem-doadores">
+              <div>
+                <p className="total-doadores">Total de doadores</p>
+                <p>
+                  <span>210</span>
+                </p>
+              </div>
+
+              <div className="maior-doador">
+                <p>Maior doador: Itaú</p>
+                <p >
+                  <span className="valor-doado">R$ 1.247.100.000</span>
+                  <span>(32%)</span>
+                </p>
+              </div>
+            </div>
+            </div>
           </div>
+          <div className="linhas"></div>
+          <div className="indicadores-subitem" >
+          <div><img className="imagem-mao-dinheiro" src={handMoney} /></div>
+            <div>
+           <div>
+              <h3>Capanhas Doações </h3>
+              <h3>
+                <span className="span-h3">R$ 278.275.247</span>
+              </h3>
+            </div>
 
-          <div className="indicadores-subitem-doadores">
-            <p>
-              Total de doadores <br />
-              <span>191.032</span>
-            </p>
+            <div className="indicadores-subitem-doadores">
+              <div>
+                <p className="total-doadores">Total de doadores</p>
+                <p>
+                  <span>191.032</span>
+                </p>
+              </div>
 
-            <p>
-              Maior Campanha: Na Luta contra a COVID-19 <br />
-              <span>R$ 38.144.000</span>
-              <span>(14%)</span>
-            </p>
+              <div className="maior-doador">
+                <p>Maior Campanha: Na Luta contra a COVID-19</p>
+                <p>
+                  <span className="valor-doado">R$ 38.144.000</span>
+                  <span>(14%)</span>
+                </p>
+              </div>
+            </div>
+            </div>
+          </div>
+    <div className="linhas"></div>
+          <div className="indicadores-subitem">
+          <div><img className="imagem-mao-dinheiro" src={handMoney} /></div>
+          <div>
+            <div>
+              <h3>Lives de Doações</h3>
+              <h3>
+                <span className="span-h3">R$ 12.258.946</span>
+              </h3>
+            </div>
+
+            <div className="indicadores-subitem-doadores">
+
+
+              <div>
+                <p className="maior-doador">Maior Live: Fome de Música (inclui Sandy&Junior) </p>
+
+                <p>
+                  <span>R$ 6.750.757</span>
+                  <span>(55%)</span>
+                </p>
+              </div>
+              </div>
+            </div>
           </div>
         </div>
-
-        <div className="indicadores-subitem">
-          <div >
-            <h3>Lives de Doações</h3> <br />
-            <span className="span-h3">R$ 12.258.946</span>
-          </div>
-
-          <div className="indicadores-subitem-doadores">
-            <p>
-              Total de doadores <br />
-              <span>191.032</span>
-            </p>
-
-            <p>
-              Maior Live: Fome de Música (inclui Sandy&Junior)
-              <br />
-              <span>R$ 6.750.757</span>
-              <span>(55%)</span>
-            </p>
-          </div>
-        </div>
-      </div>
       </div>
     </section>
   );
