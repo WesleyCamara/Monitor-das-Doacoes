ef168303e520680bd6c558888bfb8cc2f596ab8b
Rafael <rafaelrodrigues_@outlook.com>
2020-05-23 18:21:43 -0300

    Adcionado componentes para a rotas de troca de idioma

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Footer from './components/footer/Footer'
-import Menu from './components/menu/Menu'
+import { BrowserRouter } from 'react-router-dom'
+import { Routes } from './routes';
+import IntlProviderConfigured from './_i18n/IntlProviderConfigured';
+
+// import App from './App';
+// import Footer from './components/footer/Footer'
+// import Menu from './components/menu/Menu'
 
 const elemento = document.getElementById('root');
 
 ReactDOM.render(
-  <div>
-    <Menu />
-    <App />
-    <Footer />
-  </div>
+  <IntlProviderConfigured>
+    <BrowserRouter forceRefresh={true}>
+      <Routes />
+    </BrowserRouter>
+  </IntlProviderConfigured>
+  // <Switch>
+  // <Route path="/" exact={true} component={Menu} />
+  // <Route path="./App.js" component={App} />
+  // <Route path="../src/components/footer" component={Footer} />
+    
+    // <div>
+    //   <Menu />
+    //   <App />
+    //   <Footer />
+    // </div>
+    // </Switch>
+  // <BrowserRouter />
     
 , elemento);
-
-
