a30f6986b3b2df433cf1d60feb4a2a25a14cfd79
Harumi <haruumi.nishimura@gmail.com>
2020-05-23 15:51:34 -0300

    versão estatica

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import TesteGrafico from './components/doacao-setores/TesteGrafico';
+import DoacaoSetores from './components/doacao-setores/DoacaoSetores';
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
         <p>
           Monitor das doações COVID-19
         </p>
+        <DoacaoSetores/>
       </header>
     </div>
   );
--- src/components/doacao-setores/DoacaoSetores.jsx
+++ src/components/doacao-setores/DoacaoSetores.jsx
@@ -1,28 +1,135 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Highcharts from 'highcharts';
+
+const estiloSetoresContainer = {
+    width: '80%',
+    background: 'white',
+    padding: '20px'
+}
+
+const estiloBtn = {
+    background: '#8075ff',
+    border: 'none',
+    padding: '10px 35px',
+    borderRadius: '20px',
+    color: 'white',
+    fontSize: '12px',
+    fontWeight: 'bold',
+
+}
 
 class DoacaoSetores extends Component {
 
     constructor(props) {
-        super(props)
-    
+        super(props);
         this.state = {
-             
+            series: [{
+                name: 'Setores',
+                data: [
+                    {
+                      name: 'Setor Financeiro',
+                      y: 1640056334,
+                      
+                    },
+                    {
+                      name: 'Mineração',
+                      y: 612635000,
+                      
+                    },
+                    {
+                      name: 'Alimentação e Bebidas',
+                      y: 328173000,
+                     
+                    },
+                    {
+                      name: 'Educação',
+                      y: 267184223,
+                      
+                    },
+                    {
+                        name: 'Saúde',
+                        y: 165957000,
+                    
+                    },
+                    {
+                        name: 'Energia',
+                        y: 132440000,
+                    
+                    },
+                    {
+                        name: 'Famílias e indivíduos',
+                        y: 104861000,
+                    
+                    },
+                    {
+                        name: 'Comércio e Eletrônicos',
+                        y: 100000000,
+                    
+                    },
+                    {
+                        name: 'Autarquia / Funcional / Público',
+                        y: 66610000,
+                    
+                    },
+                    {
+                        name: 'Celulose',
+                        y: 62310000,
+                    
+                    },
+                    {
+                        name: 'Outros Setores (22)',
+                        y: 373500210,
+                    
+                    },
+                ]
+            }]
+           
         }
     }
-    
-    render() {
-        return (
-            <div>
-                <h1>Doação por Setores</h1>
 
-                <div id="grafico">
-                    <h2>Gráfico</h2>
-                </div>
-
-                <button>VER DETALHES</button>
-            </div>
-        )
+    highChartsRender() {
+        Highcharts.chart({
+            chart: {
+              type: 'pie',
+              renderTo: 'doacao-setores'
+            },
+            title: {
+              text: 'Doação por setores',
+              style: {
+                fontSize: '20px',
+              }
+            },
+            colors: ['#8075ff'],
+            plotOptions: {
+              pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',                
+                dataLabels: {
+                    format: '{point.name}, {point.y} '
+                }
+                
+              }
+            },
+            series: this.state.series
+        });
     }
+
+    componentDidMount() {
+        this.highChartsRender();
+    }
+
+
+
+   	render() {
+       	return (
+            <div className="container" style={estiloSetoresContainer}>
+                <div id="doacao-setores">
+                </div>
+                <button style={estiloBtn}>VER DETALHES</button>
+            </div>
+            
+       	);
+   	}
 }
 
 export default DoacaoSetores
