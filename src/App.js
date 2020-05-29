b8e4463dc8148a1857b55ed5c229f55c0705cf81
Wesley Camara <wesleycamara@hotmail.com.br>
2020-05-29 10:13:50 -0300

    Api geral funcionando

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
-import React from 'react';
+// import React from 'react';
 import './App.css';
 import Menu from './components/menu/Menu';
 import Footer from './components/footer/Footer';
+// import ComponenteClasse2 from './components/graficos-indicadores/ComponenteClasse2'
+import GraficosIndicadores from './components/graficos-indicadores/Graficos-indicadores'
 
-function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
 
-        <p>
-          Monitor das doações COVID-19
-        </p>
-      </header> */}
-      <Menu/>
-      <Footer/>
-    </div>
-  );
+
+import React, {Component} from 'react'
+import axios from 'axios';
+// import ComponenteClasse2 from './ComponenteClasse2'
+
+export default class App extends Component{
+
+    state = {
+        newData: ["VALOR INICIAL"]
+      }
+    
+      componentDidMount() {
+        axios.get('https://script.googleusercontent.com/macros/echo?user_content_key=JdkNA3_2GGI1yLyhx-9KkYn_e4McqwXBerMsDP3yW4Fkx5HuChnO1HhUZ6So0tV6Xsqi3qLV-Qlu0hSwjTn-_ghLklj8mWwUm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnFkSJ3OGPU4PNUNksnCEJmJS93T2ZzyujjUpxX3tYNvUSMYBj7AgB7_TWN7yU7wky0W-dnclfdIe&lib=MiU-jTl38wC2L3rz6MLSQoNcSVaJnOjrd')
+          .then(res => {
+            const data = res.data;
+            this.setState({ newData: data });
+          })
+      }
+
+    render(){
+
+        return ( <div className="App">
+          
+            <Menu/>
+            <GraficosIndicadores valor={this.state.newData}/>
+            <Footer/>
+            </div>
+        )
+    }
+
 }
 
-export default App;
+
+
+
+
+
+
+
+
+// function App() { usado
+//   return (
+//     <div className="App">
+//       {/* <header className="App-header">
+
+//         <p>
+//           Monitor das doações COVID-19
+//         </p>
+//       </header> */}
+//       <Menu/>
+//       <Footer/>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/components/graficos-indicadores/Graficos-indicadores.jsx
+++ src/components/graficos-indicadores/Graficos-indicadores.jsx
@@ -1,196 +1,331 @@
-import React from "react";
+import React, {Component} from 'react'
+import { FormattedMessage } from "react-intl";
+
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
+
+import handMoney from "../../assets/img/graficos-indicadores/hand-money.png";
 import "./Graficos-indicadores.css";
-import handMoney from '../../assets/img/graficos-indicadores/hand-money.png';
 
 Highcharts.setOptions({
-  lang: {
-    thousandsSep: ".",
-  },
-});
+    lang: {
+      thousandsSep: ".",
+    },
+  });
 
-const options = {
-  chart: {
-    type: "column",
-    backgroundColor: "#F3F3F3",
+export default class GraficosIndicadores extends Component{
+    constructor(props) {
+        super();
+        
+        this.state = {
+          options: {
+            chart: {
+              type: "column",
+              backgroundColor: "#F3F3F3",
+            },
+            colors: ["#4DB6AC"],
+            title: {
+              text: "",
+            },
+            plotOptions: {
+              series: {
+                groupPadding: 0,
+                borderColor: "none",
+              },
+            },
+            xAxis: {
+              type: "category",
+              lineWidth: 1,
+              lineColor: "#707070",
+              labels: {
+                rotation: 0,
+                style: {
+                  fontSize: "12px",
+                  fontFamily: "rubik, sans-serif",
+                  width: 9,
+                  textOverflow: "auto",
+                },
+              },
+            },
+            yAxis: {
+              min: 0,
+              title: {
+                text: "",
+              },
     
-  },
-  colors: ["#4DB6AC"],
-  title: {
-    text: "",
-  },
-  plotOptions: {
-    series: {
-      groupPadding: 0,
-      borderColor: 'none'
-    },
-  },
-  xAxis: {
-    type: "category",
-    lineWidth: 1,
-    lineColor: "#707070",
-    labels: {
-      rotation: 0,
-      style: {
-        fontSize: "12px",
-        fontFamily: "rubik, sans-serif",
-        width: 9,
-        textOverflow: "auto",
-      },
-    },
-  },
-  yAxis: {
-    min: 0,
-    title: {
-      text: "",
-    },
+              gridLineDashStyle: "Dash",
+              lineWidth: 2,
+              lineColor: "#222222",
+            },
+            legend: {
+              enabled: false,
+            },
+            tooltip: {
+              enable: false,
+            },
+    
+            series: [
+              {
+                name: "Valor doado",
+                data: [
+                  ["Itaú", 12],
+                  ["Vale", 13],
+                  ["Cogna", 85],
+                  ["AMBEV", 74],
+                  ["Rede D'or", 15],
+                  ["Bradesco, Itaú e Santander", 63],
+                  ["Alcoa", 48],
+                  ["Nestlé", 24],
+                  ["iFood", 10],
+                  ["BRF", 5],
+                  ["Outros Doadores (200)", 85],
+                ],
+    
+                dataLabels: {
+                  enabled: true,
+                  rotation: -90,
+                  color: "#222222",
+                  y: 65, // 10 pixels down from the top
+                  style: {
+                    fontSize: "1.25em",
+                    fontFamily: "rubik, sans-serif",
+                    fontWeight: "400",
+                    textOutline: "none",
+                  },
+                },
+              },
+            ],
+          },
+    
+          data: [],
+          totalDoado: [],
+          doadores: 0,
+          maiorDoador: [],
+          maiorValorDoado: [],
+          valor: 50,
+          API: [],
+          externo: [],
+          Show: false
+        };
 
-    gridLineDashStyle: "Dash",
-    lineWidth: 2,
-    lineColor: "#222222",
-  },
-  legend: {
-    enabled: false,
-  },
-  tooltip: {
-    enable: false,
-  },
+        
+      }
 
-  series: [
-    {
-      name: "Valor doado",
-      data: [
-        ["Itaú", 1247100000],
-        ["Vale", 500000000],
-        ["Cogna", 267000000],
-        ["AMBEV", 110000000],
-        ["Rede D'or", 108000000],
-        ["Bradesco, Itaú e Santander", 80000000],
-        ["Alcoa", 77000000],
-        ["Nestlé", 55000000],
-        ["iFood", 52000000],
-        ["BRF", 50000000],
-        ["Outros Doadores (200)", 1307626767],
-      ],
 
-      dataLabels: {
-        enabled: true,
-        rotation: -90,
-        color: "#222222",
-        y: 65, // 10 pixels down from the top
-        style: {
-          fontSize: "1.25em",
-          fontFamily: "rubik, sans-serif",
-          fontWeight: "400",
-          textOutline: "none",
-        },
-      },
-    },
-  ],
-};
+       componentDidMount() {
+        this.setState({
+            data: this.props.valor
+        })
 
-function GraficoIndicadores() {
-  return (
-    <section className="section-grafico-container">
-      <div className="grafico-indicadores">
-        <div className="div-grafico">
-          <h2 className="titulo">Empresas com mais doações</h2>
-          <div className="grafico">
-            <HighchartsReact highcharts={Highcharts} options={options} />
-          </div>
-          <div className="grafico-indicadores-botao">ver detalhes</div>
-        </div>
+          
+       
+      }
 
-        <div className="div-indicadores">
-          <h2 className="titulo">Resumo</h2>
+      componentDidUpdate() {
+        if (this.props.valor != this.state.data) {
+            this.setState({
+                data: this.props.valor
+                
+            })
+            console.log("valor",this.props.valor)
 
-          <div className="indicadores-subitem">
-          <div><img className="imagem-mao-dinheiro" src={handMoney} /></div>
-            <div>
-           <div>
-              <h3>Doações (mínimo R$3.000) </h3>
-              <h3>
-                <span className="span-h3">R$ 3.853.726.767</span>
-              </h3>
-            </div>
+            
+        this.setState({
+            totalDoado: this.formatNumber(this.props.valor.Consolidação[3][1]),
+            doadores: this.props.valor.Doações.length - 1,
+            maiorDoador: this.maiorDoador(this.props.valor.Doações),
+            maiorValorDoado: this.formatNumber(this.state.maiorDoador["Valor Anunciado"]),
+            });
 
-            <div className="indicadores-subitem-doadores">
-              <div>
-                <p className="total-doadores">Total de doadores</p>
-                <p>
-                  <span>210</span>
-                </p>
+    
+          this.setState({
+            options: {
+              series: [
+                {
+                  data: [
+                    ["Itaú", 6],
+                    ["Vale", this.state.valor],
+                    ["Cogna", 3],
+                    ["AMBEV", 4],
+                    ["Rede D'or", 5],
+                    ["Bradesco, Itaú e Santander", 6],
+                    ["Alcoa", 7],
+                    ["Nestlé", 8],
+                    ["iFood", 9],
+                    ["BRF", 10],
+                    ["Outros Doadores (200)", 11],
+                  ],
+                },
+              ],
+            },
+          });
+        }
+      }
+
+
+    
+      // Formata o número com arredondamento e formatação de ponto no milhar
+      formatNumber(number) {
+        let formattedNumber = Math.round(number).toLocaleString("pt-BR");
+        return formattedNumber;
+      }
+    
+      maiorDoador(array) {
+        let maiorDoacao = 0;
+        let maiorDoador = [];
+    
+        for (let item of array) {
+          if (
+            item["Valor Anunciado"] > maiorDoacao &&
+            item["Quem doa"] !== "Total"
+          ) {
+            maiorDoacao = item["Valor Anunciado"];
+            maiorDoador = item;
+          }
+        }
+         
+        return maiorDoador;
+      }
+    
+
+
+
+    render(){
+
+        const { options } = this.state;
+     
+        console.log(this.state.totalDoado)
+
+        return (
+         
+            <section className="section-chart-container">
+            <div className="chart-indicators">
+              <div className="div-chart">
+                <h2 className="chart-title">
+                  <FormattedMessage id="chart-indicators-chart" />
+                </h2>
+                <div className="chart">
+                  <HighchartsReact highcharts={Highcharts} options={options} />
+                </div>
+                <div className="chart-indicators-button">
+                  {" "}
+                  <FormattedMessage id="chart-indicators-button" />
+                </div>
               </div>
-
-              <div className="maior-doador">
-                <p>Maior doador: Itaú</p>
-                <p >
-                  <span className="valor-doado">R$ 1.247.100.000</span>
-                  <span>(32%)</span>
-                </p>
+    
+              <div className="div-indicators" onClick={this.handleClick}>
+                <h2 className="indicators-title">
+                  <FormattedMessage id="indicators-title" />
+                </h2>
+    
+                <div className="indicators-subitem">
+                  <div>
+                    <img className="img-hand-money" src={handMoney} />
+                  </div>
+                  <div>
+                    <div>
+                      <h3>
+                        <FormattedMessage id="indicators-donations" />{" "}
+                      </h3>
+                      <h3>
+                        <span className="span-h3">R$ {this.state.totalDoado}</span>
+                      </h3>
+                    </div>
+    
+                    <div className="indicators-subitem-doadores">
+                      <div>
+                        <p className="total-doadores">
+                          <FormattedMessage id="total-donors" />
+                        </p>
+                        <p>
+                          <span>{this.state.doadores}</span>
+                        </p>
+                      </div>
+    
+                      <div className="biggest-donor">
+                        <p>
+                          <FormattedMessage id="largest-donor" />: {this.state.maiorDoador["Quem doa"]}
+                        </p>
+                        <p>
+                          <span className="valor-doado">R$ {this.state.maiorDoador["Valor Anunciado"]}</span>
+                          <span>(00%)</span>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="linhas"></div>
+                <div className="indicators-subitem">
+                  <div>
+                    <img className="img-hand-money" src={handMoney} />
+                  </div>
+                  <div>
+                    <div>
+                      <h3>
+                        <FormattedMessage id="donations-campaigns" />
+                      </h3> 
+                      <h3>
+                        <span className="span-h3">R$ 278.275.247</span>
+                      </h3>
+                    </div>
+    
+                    <div className="indicators-subitem-doadores">
+                      <div>
+                        <p className="total-doadores">
+                          <FormattedMessage id="total-donors" />
+                        </p>
+                        <p>
+                          <span>191.032</span>
+                        </p>
+                      </div>
+    
+                      <div className="biggest-donor">
+                        <p>
+                          <FormattedMessage id="largest-campaign" />: Na Luta contra
+                          a COVID-19
+                        </p>
+                        <p>
+                          <span className="valor-doado">R$ 38.144.000</span>
+                          <span>(14%)</span>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="linhas"></div>
+                <div className="indicators-subitem">
+                  <div>
+                    <img className="img-hand-money" src={handMoney} />
+                  </div>
+                  <div>
+                    <div>
+                      <h3>
+                        <FormattedMessage id="donations-lives" />
+                      </h3>
+                      <h3>
+                        <span className="span-h3">R$ 12.258.946</span>
+                      </h3>
+                    </div>
+    
+                    <div className="indicators-subitem-doadores">
+                      <div>
+                        <p className="biggest-donor">
+                          <FormattedMessage id="biggest-live" />: Fome de Música
+                          (inclui Sandy&Junior){" "}
+                        </p>
+    
+                        <p>
+                          <span>R$ 6.750.757</span>
+                          <span>(55%)</span>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            </div>
-          </div>
-          <div className="linhas"></div>
-          <div className="indicadores-subitem" >
-          <div><img className="imagem-mao-dinheiro" src={handMoney} /></div>
-            <div>
-           <div>
-              <h3>Capanhas Doações </h3>
-              <h3>
-                <span className="span-h3">R$ 278.275.247</span>
-              </h3>
-            </div>
+          </section>
+        )
+    }
 
-            <div className="indicadores-subitem-doadores">
-              <div>
-                <p className="total-doadores">Total de doadores</p>
-                <p>
-                  <span>191.032</span>
-                </p>
-              </div>
-
-              <div className="maior-doador">
-                <p>Maior Campanha: Na Luta contra a COVID-19</p>
-                <p>
-                  <span className="valor-doado">R$ 38.144.000</span>
-                  <span>(14%)</span>
-                </p>
-              </div>
-            </div>
-            </div>
-          </div>
-    <div className="linhas"></div>
-          <div className="indicadores-subitem">
-          <div><img className="imagem-mao-dinheiro" src={handMoney} /></div>
-          <div>
-            <div>
-              <h3>Lives de Doações</h3>
-              <h3>
-                <span className="span-h3">R$ 12.258.946</span>
-              </h3>
-            </div>
-
-            <div className="indicadores-subitem-doadores">
-
-
-              <div>
-                <p className="maior-doador">Maior Live: Fome de Música (inclui Sandy&Junior) </p>
-
-                <p>
-                  <span>R$ 6.750.757</span>
-                  <span>(55%)</span>
-                </p>
-              </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
 }
-
-export default GraficoIndicadores;
