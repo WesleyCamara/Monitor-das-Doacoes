a47046d52fe6051810eb015ac56b678c8e41fb3f
Rafael <rafaelrodrigues_@outlook.com>
2020-06-01 12:52:47 -0300

    Alteração URL API

--- src/services/API.js
+++ src/services/API.js
@@ -2,7 +2,7 @@
 import Axios from "axios";
 
 const api =  Axios.create( {
-  baseURL: `https://script.googleusercontent.com/macros/echo?user_content_key=Q_9Dq5-x6AKhozZ-Pr4RARJLq0xr1B-TEKmmvM_UyPCjvDLJ4dS8Pxya0YI6uedx3kD9YDME9wkwc2Y1Ufgv1VUAEGiYXU3_m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnFkSJ3OGPU4PNUNksnCEJmJS93T2ZzyujjUpxX3tYNvUSMYBj7AgB7_TWN7yU7wky0W-dnclfdIe&lib=MiU-jTl38wC2L3rz6MLSQoNcSVaJnOjrd`
+  baseURL: "https://script.googleusercontent.com/macros/echo?user_content_key=HHPgHY0VTjUOtvbiM59KZSfpXISWkOsv6VDGAbI-16-RHELWJOk66ERsKhQ73D6Z7ohS8jGm_iyH3nHl8n7O4S0WuK_EtGHWm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnFkSJ3OGPU4PNUNksnCEJmJS93T2ZzyujjUpxX3tYNvUSMYBj7AgB7_TWN7yU7wky0W-dnclfdIe&lib=MiU-jTl38wC2L3rz6MLSQoNcSVaJnOjrd"
   
 });
 
