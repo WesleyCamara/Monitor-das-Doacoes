f61e8c94bd52f8598ea78d8ffe8aa6398567d9ac
Wesley Camara <wesleycamara@hotmail.com.br>
2020-06-03 15:11:35 -0300

    migração dolar finalizada

--- src/components/graficos-indicadores/Graficos-indicadores-Hook.jsx
+++ src/components/graficos-indicadores/Graficos-indicadores-Hook.jsx
@@ -4,8 +4,34 @@ import { FormattedMessage } from "react-intl";
 import handMoney from "../../assets/img/graficos-indicadores/hand-money.png";
 import HighchartsReact from "highcharts-react-official";
 import Highcharts from "highcharts";
+import axios from "axios";
 
 const GraficosIndicadores = (props) => {
+const moeda = {
+  valorAnunciado : "Valor Anunciado",
+  valorDoado: "Valor Doado",
+  acessoIndiceTotal: 1,
+  acessoIndiceLives: 5,
+  simbolo: "R$",
+  valorDoadoLabel: "Valor doado 123"
+}
+
+
+
+  // const buscaDolar = async () => {
+  //   await axios.get("https://economia.awesomeapi.com.br/all/USD-BRL").then((res) => {
+  //     const valorDolar = res.data.USD.bid
+  //     console.log(valorDolar);
+
+  //     return valorDolar
+  //   });
+  // };
+
+  Highcharts.setOptions({
+    lang: {
+      thousandsSep: ".",
+    },
+  });
 
   const [valores, setValores] = useState({
     total: 0,
@@ -18,43 +44,41 @@ const GraficosIndicadores = (props) => {
     maiorDoacao: 0,
     totalDoadoresCampanhas: 0,
     doacoesOrdenadas: [],
-
   });
 
-
-
-// Atualiza os dados que serão tratados, somente quando recebe os valores das props 
+  // Atualiza os dados que serão tratados, somente quando recebe os valores das props
   useEffect(() => {
-    if (props.valor.status == "ok" && valores.maiorCampanha == 0) {
+    if (props.valor.status === "ok" && valores.maiorCampanha === 0) {
       setValores({
         maiorDoador: filtraMaiorDoador(props.valor["Doações"]),
         maiorCampanha: filtraMaiorCampanha(props.valor["Campanhas"]),
         maiorLive: maiorLive(props.valor["Lives"]),
-        total: props.valor["Consolidação"][4][1],
-        totalCampanhas: props.valor["Consolidação"][2][1],
-        totalLives: props.valor["Consolidação"][3][1],
+        total: props.valor["Consolidação"][4][moeda.acessoIndiceTotal],
+        totalCampanhas: props.valor["Consolidação"][2][moeda.acessoIndiceTotal],
+        totalLives: props.valor["Consolidação"][3][moeda.acessoIndiceTotal],
         totalDoadores: props.valor["Doações"].length - 2,
         totalDoadoresCampanhas: subtrai(
           props.valor["Consolidação"][6][1],
           valores.totalDoadores
         ),
         doacoesOrdenadas: ordenaDoacoes(props.valor["Doações"]),
-  
       });
+      
       
     }
   }, [props, valores.maiorCampanha]);
 
-
-
-// Se a array com os valores para o gráfico estiver pronta, chama a atualização dele 
+  // Se a array com os valores para o gráfico estiver pronta, chama a atualização dele
   useEffect(() => {
-    if(valores.doacoesOrdenadas.length > 0 && valores.doacoesOrdenadas[0].doador.length > 0){
-    updateSeries();
-  }
-}, [valores.doacoesOrdenadas]  )
-
-
+    if (
+      valores.doacoesOrdenadas.length > 0 &&
+      valores.doacoesOrdenadas[0].doador.length > 0
+    ) {
+      formatValue()
+      updateSeries();
+      console.log("valor", moeda.valorAnunciado)
+    }
+  }, [valores.doacoesOrdenadas]);
 
   // Formata o número com arredondamento e adiciona os pontos nos milhares
   const formatNumber = (number) => {
@@ -69,21 +93,21 @@ const GraficosIndicadores = (props) => {
 
     for (let item of array) {
       if (
-        item["Valor Anunciado"] > maiorDoacao &&
+        item[moeda.valorAnunciado] > maiorDoacao &&
         item["Quem doa"] !== "Total"
       ) {
-        maiorDoacao = item["Valor Anunciado"];
+        maiorDoacao = item[moeda.valorAnunciado];
         maiorDoador = item;
       }
     }
     return maiorDoador;
   };
 
-
-    // filtra a campanha com maior arrecadação 
+  // filtra a campanha com maior arrecadação
   const filtraMaiorCampanha = (array) => {
     let maiorDoacao = 0;
     let maiorDoador = [];
+
 
     for (let item of array) {
       if (
@@ -103,8 +127,7 @@ const GraficosIndicadores = (props) => {
     return itemMaior - itemMenor;
   };
 
-
-  // filtra a live com maior arrecadação 
+  // filtra a live com maior arrecadação
   const maiorLive = (array) => {
     let maiorDoacao = 0;
     let maiorLive = [];
@@ -122,34 +145,31 @@ const GraficosIndicadores = (props) => {
     return maiorLive;
   };
 
-  // Calcula a porcentagem de uma valor sobre o outro 
+  // Calcula a porcentagem de uma valor sobre o outro
   const porcentagem = (parte, total) => {
     return Math.round((parte * 100) / total);
   };
 
-
-  // Ordena as doações pelo maior valor e retorna as 11 maiores 
+  // Ordena as doações pelo maior valor e retorna as 11 maiores
   const ordenaDoacoes = (doacoes) => {
     let maioresDoacoes = [];
 
     let saida = doacoes.sort(function (a, b) {
-      return b["Valor Anunciado"] - a["Valor Anunciado"];
+      return b[moeda.valorAnunciado] - a[moeda.valorAnunciado];
     });
 
     for (let item = 2; item < 13; item++) {
       maioresDoacoes.push({
         doador: saida[item]["Quem doa"],
-        valorDoado: saida[item]["Valor Anunciado"],
+        valorDoado: saida[item][moeda.valorAnunciado],
       });
     }
-    console.log(maioresDoacoes)
+
 
     return maioresDoacoes;
-    
-  }
+  };
 
-
-    // Opções do gráfico 
+  // Opções do gráfico
   const [chartOptions, setChartOptions] = useState({
     chart: {
       type: "column",
@@ -198,16 +218,14 @@ const GraficosIndicadores = (props) => {
 
     series: [
       {
-        name: "Valor doado",
-        data: [
-
-        ],
+        name: moeda.valorDoadoLabel,
+        data: [],
 
         dataLabels: {
           enabled: true,
           rotation: -90,
           color: "#222222",
-          y: 55, // 10 pixels down from the top
+          y: 50, // 10 pixels down from the top
           style: {
             fontSize: "1.25em",
             fontFamily: "rubik, sans-serif",
@@ -219,32 +237,79 @@ const GraficosIndicadores = (props) => {
     ],
   });
 
-
-  // Atualiza as series do gráfico, é alimentado pela const doacoesOrdenadas e chamado quando essa const está pronta 
+  // Atualiza as series do gráfico, é alimentado pela const doacoesOrdenadas e chamado quando essa const está pronta
   const updateSeries = () => {
     setChartOptions({
       series: [
         {
           data: [
-            [valores.doacoesOrdenadas[0]["doador"], valores.doacoesOrdenadas[0]["valorDoado"]],
-            [valores.doacoesOrdenadas[1]["doador"], valores.doacoesOrdenadas[1]["valorDoado"]],
-            [valores.doacoesOrdenadas[2]["doador"], valores.doacoesOrdenadas[2]["valorDoado"]],
-            [valores.doacoesOrdenadas[3]["doador"], valores.doacoesOrdenadas[3]["valorDoado"]],
-            [valores.doacoesOrdenadas[4]["doador"], valores.doacoesOrdenadas[4]["valorDoado"]],
-            [valores.doacoesOrdenadas[5]["doador"], valores.doacoesOrdenadas[5]["valorDoado"]],
-            [valores.doacoesOrdenadas[6]["doador"], valores.doacoesOrdenadas[6]["valorDoado"]],
-            [valores.doacoesOrdenadas[7]["doador"], valores.doacoesOrdenadas[7]["valorDoado"]],
-            [valores.doacoesOrdenadas[8]["doador"], valores.doacoesOrdenadas[8]["valorDoado"]],
-            [valores.doacoesOrdenadas[9]["doador"], valores.doacoesOrdenadas[9]["valorDoado"]],
-            [valores.doacoesOrdenadas[10]["doador"], valores.doacoesOrdenadas[10]["valorDoado"]],
+            [
+              valores.doacoesOrdenadas[0]["doador"],
+              Math.round(valores.doacoesOrdenadas[0]["valorDoado"]),
+            ],
+            [
+              valores.doacoesOrdenadas[1]["doador"],
+              Math.round(valores.doacoesOrdenadas[1]["valorDoado"]),
+            ],
+            [
+              valores.doacoesOrdenadas[2]["doador"],
+              Math.round(valores.doacoesOrdenadas[2]["valorDoado"]),
+            ],
+            [
+              valores.doacoesOrdenadas[3]["doador"],
+              Math.round(valores.doacoesOrdenadas[3]["valorDoado"]),
+            ],
+            [
+              valores.doacoesOrdenadas[4]["doador"],
+              Math.round(valores.doacoesOrdenadas[4]["valorDoado"]),
+            ],
+            [
+              valores.doacoesOrdenadas[5]["doador"],
+              Math.round(valores.doacoesOrdenadas[5]["valorDoado"]),
+            ],
+            [
+              valores.doacoesOrdenadas[6]["doador"],
+              Math.round(valores.doacoesOrdenadas[6]["valorDoado"]),
+            ],
+            [
+              valores.doacoesOrdenadas[7]["doador"],
+              Math.round(valores.doacoesOrdenadas[7]["valorDoado"]),
+            ],
+            [
+              valores.doacoesOrdenadas[8]["doador"],
+              Math.round(valores.doacoesOrdenadas[8]["valorDoado"]),
+            ],
+            [
+              valores.doacoesOrdenadas[9]["doador"],
+              Math.round(valores.doacoesOrdenadas[9]["valorDoado"]),
+            ],
+            [
+              valores.doacoesOrdenadas[10]["doador"],
+              Math.round(valores.doacoesOrdenadas[10]["valorDoado"]),
+            ],
           ],
         },
       ],
     });
   };
 
+  const formatValue = () => {
+    const url_atual = window.location.pathname;
+    if (url_atual !== "/pt") {
+      moeda.valorAnunciado = "in Dollars"
+      moeda.valorDoado = "in Dollars"
+      moeda.acessoIndiceTotal = 2
+      moeda.acessoIndiceLives = 6
+      moeda.simbolo = "$"
+      moeda.valorDoadoLabel = "Donated amount"
+    } 
+ 
+  };
+
   return (
+    
     <>
+    {formatValue()}
       <section className="section-chart-container">
         <div className="chart-indicators">
           <div className="div-chart">
@@ -252,10 +317,8 @@ const GraficosIndicadores = (props) => {
               <FormattedMessage id="chart-indicators-chart" />
             </h2>
             <div className="chart">
-
               {/* Import do gráfico  */}
               <HighchartsReact highcharts={Highcharts} options={chartOptions} />
-
             </div>
 
             <a
@@ -285,7 +348,7 @@ const GraficosIndicadores = (props) => {
                   </h3>
                   <h3>
                     <span className="span-h3">
-                      R$ {formatNumber(valores.total)}{" "}
+                      {moeda.simbolo} {formatNumber(valores.total)}{" "}
                     </span>
                   </h3>
                 </div>
@@ -307,12 +370,13 @@ const GraficosIndicadores = (props) => {
                     </p>
                     <p>
                       <span className="valor-doado">
-                        R${formatNumber(valores.maiorDoador["Valor Anunciado"])}
+                        {moeda.simbolo}{formatNumber(valores.maiorDoador[moeda.valorAnunciado])}{" "}
+                       
                       </span>
                       <span>
                         (
                         {porcentagem(
-                          Number(valores.maiorDoador["Valor Anunciado"]),
+                          Number(valores.maiorDoador[moeda.valorAnunciado]),
                           Number(valores.total)
                         )}
                         )%
@@ -334,7 +398,7 @@ const GraficosIndicadores = (props) => {
                   </h3>
                   <h3>
                     <span className="span-h3">
-                      R$ {formatNumber(valores.totalCampanhas)}
+                      {moeda.simbolo} {formatNumber(valores.totalCampanhas)}
                     </span>
                   </h3>
                 </div>
@@ -346,7 +410,7 @@ const GraficosIndicadores = (props) => {
                     </p>
                     <p>
                       <span>
-                        {formatNumber(valores.totalDoadoresCampanhas)}
+                        {formatNumber(valores.totalDoadoresCampanhas - valores.totalDoadores)}
                       </span>
                     </p>
                   </div>
@@ -357,13 +421,14 @@ const GraficosIndicadores = (props) => {
                       {valores.maiorCampanha["Campanhas"]}
                     </p>
                     <p>
-                      <span className="valor-doado">
-                        R${formatNumber(valores.maiorCampanha["Valor Doado"])}
+                      {/* voltar isso pra valor doado----------------------------------- */}
+                      <span className="valor-doado"> 
+                        {moeda.simbolo}{formatNumber(valores.maiorCampanha[moeda.valorDoado])}
                       </span>
                       <span>
                         (
                         {porcentagem(
-                          Number(valores.maiorCampanha["Valor Doado"]),
+                          Number(valores.maiorCampanha[moeda.valorDoado]),
                           Number(valores.totalCampanhas)
                         )}
                         )%
@@ -385,7 +450,7 @@ const GraficosIndicadores = (props) => {
                   </h3>
                   <h3>
                     <span className="span-h3">
-                      R$ {formatNumber(valores.totalLives)}
+                      {moeda.simbolo} {formatNumber(valores.totalLives)}
                     </span>
                   </h3>
                 </div>
@@ -393,16 +458,15 @@ const GraficosIndicadores = (props) => {
                 <div className="indicators-subitem-doadores">
                   <div>
                     <p className="biggest-donor">
-                      <FormattedMessage id="biggest-live" />:{" "}
-                      {valores.maiorLive[1]}
+                      <FormattedMessage id="biggest-live" />:{valores.maiorLive[1]}
                     </p>
 
                     <p>
-                      <span>R$ {formatNumber(valores.maiorLive[5])}</span>
+                      <span>{moeda.simbolo} {formatNumber(valores.maiorLive[moeda.acessoIndiceLives])}</span>
                       <span>
                         (
                         {porcentagem(
-                          Number(valores.maiorLive[5]),
+                          Number(valores.maiorLive[moeda.acessoIndiceLives]),
                           Number(valores.totalLives)
                         )}
                         )%
