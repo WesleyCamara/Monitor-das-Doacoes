df2ecbf9a61145831ad0e76202b7bc32dff1da72
Wesley Camara <wesleycamara@hotmail.com.br>
2020-06-05 00:25:14 -0300

    reescrita desktop finalizada

--- src/components/blocoinfodois/Blocoinfodois.jsx
+++ src/components/blocoinfodois/Blocoinfodois.jsx
@@ -14,13 +14,29 @@ import doar from '../../assets/img/blocoinfo/doar.jpg';
 export default class BlocoInfoDois extends Component {
     render() {
         return ( <>
-                <div className="blocoinfodois">
-                    <div className="container-parteprincipal">
-                        <div className="container-texto">
+    <div className="blocodois">
+                    <div className="container-fundo">
+                    
                             <div className="fundo-sombra">
                                 <img src={monitor} />
                             </div>
+
+                            <div className="imgdecorativo">
+                            <img src={imgcanto} />
+                             </div>
+
+                            <div className="quadrado"></div>
+
+                             <div className="circulo">
+                            <img src={circles} />
                         </div>
+
+
+
+                    </div>  
+
+                    <div className="container-texto">
+                        
                         <div className="textotitulo">
                             <h1><FormattedMessage id="tituloumbldois" /><br /><FormattedMessage id="titulodoisbldois" /></h1>
                             <p><FormattedMessage id="text1" /><br />
@@ -34,45 +50,55 @@ export default class BlocoInfoDois extends Component {
                                 <FormattedMessage id="text9" /><br />
                                 <FormattedMessage id="text10" />
                             </p>
+
+                            <div className="botao">
+                           <a className="botao-h1" href="https://docs.google.com/spreadsheets/d/1RA0oP9EBHxpsLGvHTaX2TTYHT2oQHTfNrM8Z40hqVus/edit#gid=0" target="_blank"><FormattedMessage id= "pessoasdoando" /></a>
+                            </div>
                         </div>
-                        <div className="botao">
-                            <h1><a href="https://docs.google.com/spreadsheets/d/1RA0oP9EBHxpsLGvHTaX2TTYHT2oQHTfNrM8Z40hqVus/edit#gid=0" target="_blank"><FormattedMessage id= "pessoasdoando" /></a></h1>
-                        </div>
-                        <div className="imgdecorativo">
-                            <img src={imgcanto} />
-                        </div>
-                        <div className="circulo">
-                            <img src={circles} />
-                        </div>
+
+
+                       
+                     <div className="container-qr">
+                    <div className="container-qr-qrcode">
                         <div className="qrcode">
                         <a href="http://bi.anna.center/w5/tdaportalclient.aspx" target="_blank"><img src={code} /> </a>
-                            <p><FormattedMessage id="textcode" /><br />
-                            <FormattedMessage id="textcodeum" /><br />
-                            <FormattedMessage id="textcodedois" /></p>
+                          
+
+                        </div> 
+                        
                         <div className="seta">
-                            <img src={seta} />
+                        <p><FormattedMessage id="textcode" /><br />
+                            <FormattedMessage id="textcodeum" /><br />
+                            <FormattedMessage id="textcodedois" /><img src={seta} /></p>
+                            
                         </div>
+                        
+                        </div>
+
                         <div className="textbaixo">
                             <p><FormattedMessage id="textinferior" /><br />
-                            <a href='https://docs.google.com/spreadsheets/d/1RA0oP9EBHxpsLGvHTaX2TTYHT2oQHTfNrM8Z40hqVus/edit#gid=0' target="_blank"><FormattedMessage id="textinferiorum" /> <FormattedMessage id="textinferiordois" /></a></p>
-                        </div> 
-                    </div>       
-                </div> 
-            </div>
+                          <FormattedMessage id="textinferiorum" />   <a href='https://docs.google.com/spreadsheets/d/1RA0oP9EBHxpsLGvHTaX2TTYHT2oQHTfNrM8Z40hqVus/edit#gid=0' target="_blank"><FormattedMessage id="textinferiordois" /></a></p>
+                        
+                        </div>
+                        </div>
+                         
+                    </div>
+               
+          
 
-            <div className="blocoinfotres">
+
                 <div className="container-final">
                     <div className="doar">
                         <img src={doar} />
                     </div>
                     <div className="textoemail">
                         <p><FormattedMessage id="textemail1" /><br />
-                            <FormattedMessage id="textemail2" /> <a href="malito:abcr@captadores.org.br" target="_blank"><FormattedMessage id="textemail3" /></a><br />
+                            <FormattedMessage id="textemail2" /> <a className="email" href="malito:abcr@captadores.org.br" target="_blank"><FormattedMessage id="textemail3" /></a><br />
                             <FormattedMessage id="textemail4" /><br />
                             <FormattedMessage id="textemail5" /></p>
                     </div>
                 </div>
-            </div>
+                </div>
             </>
         );
     }
