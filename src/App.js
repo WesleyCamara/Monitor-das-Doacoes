8cd2b6d9e0d5d4e6ed10ada3a92644c903ecb046
Wesley Camara <wesleycamara@hotmail.com.br>
2020-06-05 03:17:12 -0300

    componentes integrados

--- src/App.js
+++ src/App.js
@@ -31,12 +31,12 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-         {/* <Menu />
+         <Menu />
         <ChamadaInicialIndicadores />
         <ListaDoadores valor={this.state.newData["Doações"]}/>
         <DoacaoSetoresHook valor={this.state.newData} />
         <GraficosIndicadores valor={this.state.newData} />
-        <Blocoinfo valor={this.state.newData}/> */}
+        <Blocoinfo valor={this.state.newData}/>
         <BlocoInfoDois />
         <Footer />
       </div>
