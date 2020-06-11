ef8e9c6558c08f7b583891fcc4d1a5365d962793
thiago dutra <thiago.silva2210@outlook.com>
2020-06-11 02:56:22 -0300

    final da correção e inclusão das nuvens

--- src/components/chamada-inicial-indicadores/Chamada-inicial-indicadores.jsx
+++ src/components/chamada-inicial-indicadores/Chamada-inicial-indicadores.jsx
@@ -3,8 +3,8 @@ import React, { useState, useEffect } from "react";
 import "./Chamada-inicial-indicadores.css";
 import money from "../../assets/img/chamada-inicial-indicadores/money-bill.png";
 import hand from "../../assets/img/chamada-inicial-indicadores/hand-holding-money.png";
-import nv1 from "../../assets/img/chamada-inicial-indicadores/Caminho49.svg";
-import nv2 from "../../assets/img/chamada-inicial-indicadores/Caminho59.svg";
+import nv2 from "../../assets/img/chamada-inicial-indicadores/Caminho49.svg";
+import nv1 from "../../assets/img/chamada-inicial-indicadores/Caminho59.svg";
 import centerImg from "../../assets/img/chamada-inicial-indicadores/Grupo1.svg";
 import loading from "../../assets/img/chamada-inicial-indicadores/loading.gif";
 import { FormattedMessage } from "react-intl";
@@ -169,11 +169,12 @@ const ChamadaInicialIndicadores = (props) => {
           <div id="banner-content">
             {/*-----div-to-center-items------*/}
             <div id="money">
+              <div id="nvd">
+                <img id="nv1" src={nv1} alt="nuvem" />
+              </div>
               {/*-----doações-recebidas-----*/}
-              <img src={money} alt="quantidade doada" />
-              <img id="nv1" src={nv1} alt="nuvem" />
+              <img id="doa" src={money} alt="quantidade doada" />
               {/*-----"doacoes"->receberá-dados-da-api-qtd-de--doações*/}
-              <div className="nvs2"></div>
               <div id="doacoes" style={visible.number}>
                 {moeda.simbolo} {formatNumber(valores.total)}
               </div>
@@ -185,10 +186,11 @@ const ChamadaInicialIndicadores = (props) => {
               </h2>
             </div>
             <div id="hand">
+              <div id="nve">
+                <img id="nv2" src={nv2} alt="nuvem" />
+              </div>
               {/*-----doadores------*/}
-              <img src={hand} alt="doadores" />
-              <img id="nv2" src={nv2} alt="nuvem" />
-              <div className="nvs1"></div>
+              <img id="hand-p" src={hand} alt="doadores" />
               {/*-----"doadores"->receberá-dados-da-api-qtd-doadores----*/}
               <div id="doadores" style={visible.number}>
                 {formatNumber(valores.totalDoadores)}
