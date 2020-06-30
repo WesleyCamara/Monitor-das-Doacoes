d8fc9e2f3bfc6e09b18045ac833b5362f8a9c0e3
Rafael <rafaelrodrigues_@outlook.com>
2020-06-30 00:19:55 -0300

    Aumento tamanho das fontes e mudança na cor dos links

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,28 +1,35 @@
 import React, { Component } from "react";
-import './Menu.css';
-import { Link, NavLink } from "react-router-dom";
+import "./Menu.css";
+import { NavLink } from "react-router-dom";
 import { FormattedMessage } from "react-intl";
 
+import logo from "../../assets/img/lista-doadores/ABCR_Logo POSITIVO.png";
+
 export default class Menu extends Component {
-    render() {
-        return (
-            <div className="container-menu">
-                <div className="menu">
-                    <div className="menu-title">
-                        <h1>
-                        <FormattedMessage id="menu-title" />
-                        </h1>
-                    </div>
-                    <div className="menu-language">
-                        <div className="menu-pt">
-                            <NavLink to="/pt" activeClassName="active">PT</NavLink>
-                        </div>
-                        <div className="menu-en">
-                            <NavLink to="/en" activeClassName="active">EN</NavLink>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="container-menu">
+        <div className="menu">
+          <div className="menu-title">
+            <img src={logo} alt="Logo" />
+            <h1>
+              <FormattedMessage id="menu-title" />
+            </h1>
+          </div>
+          <div className="menu-language">
+            <div className="menu-pt">
+              <NavLink to="/pt" activeClassName="active">
+                PT
+              </NavLink>
             </div>
-        )
-    }
+            <div className="menu-en">
+              <NavLink to="/en" activeClassName="active">
+                EN
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
