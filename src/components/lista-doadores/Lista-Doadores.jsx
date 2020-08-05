5a8a2582649c8806f637f3743464a24b886ac597
Rafael <rafaelrodrigues_@outlook.com>
2020-08-05 15:33:32 -0300

    Quantidade de ítens exibidos na lista de doadores aumentada para 50

--- src/components/lista-doadores/Lista-Doadores.jsx
+++ src/components/lista-doadores/Lista-Doadores.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import "./Lista-Doadores.css";
-import Loading from "../../assets/img/lista-doadores/loading.svg";
-import shape from "../../assets/img/lista-doadores/square-shapes.svg";
-import { FormattedMessage } from "react-intl";
+import React, { Component } from 'react';
+import './Lista-Doadores.css';
+import Loading from '../../assets/img/lista-doadores/loading.svg';
+import shape from '../../assets/img/lista-doadores/square-shapes.svg';
+import { FormattedMessage } from 'react-intl';
 
 export default class ListaDoadores extends Component {
   constructor(props) {
@@ -10,7 +10,7 @@ export default class ListaDoadores extends Component {
     this.state = {
       lista: [],
       doacao: [],
-      num: 30,
+      num: 50,
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -28,8 +28,8 @@ export default class ListaDoadores extends Component {
   // Acrescenta mais 30 resultados na lista de doadores
   moreResults = () => {
     this.setState({
-      num: this.state.num + 30,
-      doacao: []
+      num: this.state.num + 50,
+      doacao: [],
     });
   };
 
@@ -40,11 +40,11 @@ export default class ListaDoadores extends Component {
 
   // Define o formato do valor da doação entre real e dolar dependendo do idioma escolhido
   formatValue(value) {
-    if (this.url_atual !== "/en") {
-      let formattedValue = Math.round(value).toLocaleString("pt-BR");
+    if (this.url_atual !== '/en') {
+      let formattedValue = Math.round(value).toLocaleString('pt-BR');
       return formattedValue;
     } else {
-      let formattedValue = Math.round(value).toLocaleString("en-US");
+      let formattedValue = Math.round(value).toLocaleString('en-US');
       return formattedValue;
     }
   }
@@ -76,14 +76,14 @@ export default class ListaDoadores extends Component {
     lista &&
       lista.map((donation) => {
         if (
-          donation["Quem doa"] !== "Quem doa" &&
-          donation["Quem doa"] !== "Total"
+          donation['Quem doa'] !== 'Quem doa' &&
+          donation['Quem doa'] !== 'Total'
         ) {
           this.doacaoObject.push({
-            quemdoa: donation["Quem doa"],
-            valor: donation["Valor Anunciado"],
-            dollar: donation["in Dollars"],
-            referencia: donation["Referência"],
+            quemdoa: donation['Quem doa'],
+            valor: donation['Valor Anunciado'],
+            dollar: donation['in Dollars'],
+            referencia: donation['Referência'],
           });
           this.doacaoObject.sort(function (a, b) {
             return a.quemdoa > b.quemdoa ? 1 : b.quemdoa > a.quemdoa ? -1 : 0;
@@ -95,7 +95,7 @@ export default class ListaDoadores extends Component {
     this.doacaoObject.map((donation) => {
       if (
         this.state.doacao.length < this.state.num &&
-        this.url_atual !== "/en"
+        this.url_atual !== '/en'
       ) {
         this.setState.doacao = this.state.doacao.push({
           quemdoa: donation.quemdoa,
@@ -116,17 +116,21 @@ export default class ListaDoadores extends Component {
       this.listaDoadores =
         this.state.doacao &&
         this.state.doacao.map((donation, index) => (
-          <div key={"donations" + index}>
+          <div key={'donations' + index}>
             <p>
               <span className="donation">
-                {" "}
-                <a href={donation.referencia} target="_blank" rel="noopener noreferrer">
-                  {" "}
-                  {donation.quemdoa}{" "}
-                </a>{" "}
-                -{" "}
-              </span>{" "}
-              <FormattedMessage id="donation-value" />{" "}
+                {' '}
+                <a
+                  href={donation.referencia}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {' '}
+                  {donation.quemdoa}{' '}
+                </a>{' '}
+                -{' '}
+              </span>{' '}
+              <FormattedMessage id="donation-value" />{' '}
               {this.formatValue(donation.valor)}
             </p>
           </div>
@@ -140,13 +144,13 @@ export default class ListaDoadores extends Component {
             <div className="donations-top-text">
               <div className="donations-top-title">
                 <h2>
-                  <FormattedMessage id="donations-top-title" />{" "}
+                  <FormattedMessage id="donations-top-title" />{' '}
                 </h2>
               </div>
               <div className="donations-top-description">
                 <p>
                   <FormattedMessage id="donations-top-description" />
-                  {this.amount() || "0"}
+                  {this.amount() || '0'}
                   <FormattedMessage id="donations-top-description-two" />
                 </p>
               </div>
@@ -159,7 +163,7 @@ export default class ListaDoadores extends Component {
             <div className="lista">
               {this.listaDoadores || (
                 <div className="loading">
-                  <img src={Loading} alt="Carregando..."/>
+                  <img src={Loading} alt="Carregando..." />
                 </div>
               )}
             </div>
