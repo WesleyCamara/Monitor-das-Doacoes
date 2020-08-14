14144328d17797f9de4493276e5d1cc4cbb35046
Wesley Camara <wesleycamara@hotmail.com.br>
2020-08-14 18:29:42 -0300

    ranking responsivo

--- src/App.js
+++ src/App.js
@@ -33,16 +33,16 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-<Menu />
+        <Menu />
         <ChamadaInicialIndicadores valor={this.state.newData} />
         <ListaDoadores valor={this.state.newData["Doações"]} />
         <EvolucaoSemanal valor={this.state.newData} />
         <GraficoTipoDoacao valor={this.state.newData} />
         <DoacaoSetoresHook valor={this.state.newData} />
         <GraficosIndicadores valor={this.state.newData} />
-        <Ranking valor={this.state.newData}/>
-        <BlocoInfoDois /> 
-        <Footer />
+        <Ranking valor={this.state.newData} />
+        {/* <BlocoInfoDois /> 
+        <Footer /> */}
       </div>
     );
   }
